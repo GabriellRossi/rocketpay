38057aef0b9b5ae6eae064041710984adafd436c
Gabriel Rossi <gabriel.rossi159@gmail.com>
2022-10-21 22:49:58 -0300

    Criado evento de clique e mudar o nome no cartão ao digitar

--- src/main.js
+++ src/main.js
@@ -80,3 +80,18 @@ const cardNumberPattern = {
   },
 };
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
+
+const addButton = document.querySelector("#add-card");
+addButton.addEventListener("click", () => {});
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+const cardHolder = document.querySelector("#card-holder");
+cardHolder.addEventListener("input", () => {
+  const ccHolder = document.querySelector(".cc-holder .value");
+
+  ccHolder.innerText =
+    cardHolder.value.lenght === 0 ? "FULANO DA SILVA" : cardHolder.value;
+});
